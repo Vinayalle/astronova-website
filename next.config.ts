af75d139058cf498369a7ee0b18804c187e1193c
vinay <95430008+Vinayalle@users.noreply.github.com>
2025-08-09 14:57:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  output: 'export',
+  
   
  images: {
   domains: ["upload.wikimedia.org", "images.pexels.com", "cdn.pixabay.com"],
-  unoptimized: true,
+  
 }
 
 };
